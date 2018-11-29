d91c5cf270c3c81fb6ae13ef25cb5a00518549dc
Pål Driveklepp <pauldriveklepp@gmail.com>
2018-11-29 21:56:09 +0100

    Run tests with valgrind

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-clang++ --std=c++11 test_mbusparser.cpp libraries/mbusparser/mbusparser.cpp -o test_mbusparser && ./test_mbusparser
+clang++ --std=c++11 test_mbusparser.cpp libraries/mbusparser/mbusparser.cpp -o test_mbusparser && valgrind ./test_mbusparser
